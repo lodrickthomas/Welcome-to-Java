088393b6cfa17cacd12de16d4fccea1a3bdc4b5c
Lodrick_Tz <complexlodrick@gmail.com>
2017-11-04 00:18:23 +0300

    New lines added

---
+++ src/com/javaone/Main.java
@@ -0,0 +1,11 @@
+package com.javaone;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hellow git and git hub");
+        System.out.println("Hellow git and git hub");
+        System.out.println("Hellow git and git hub 222");
+    }
+}
