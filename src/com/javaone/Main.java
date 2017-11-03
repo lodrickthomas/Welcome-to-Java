f33bd46e407590971b9ba8f1e1355202bf9e30fc
Lodrick_Tz <complexlodrick@gmail.com>
2017-11-04 00:49:27 +0300

    added another Class and remove unwanted statements

--- src/com/javaone/Main.java
+++ src/com/javaone/Main.java
@@ -5,7 +5,23 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hellow git and git hub");
-        System.out.println("Hellow git and git hub");
-        System.out.println("Hellow git and git hub 222");
+
+        System.out.println("calling a method: " + names.list("lodrick"));
+
+        //call a static class and method
+
+
     }
+}
+
+class names{
+    public static String list(String name){
+
+        return name;
+
+
+    }
+
+
+
 }
